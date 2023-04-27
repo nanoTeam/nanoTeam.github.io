142aefc5054f2020b7aa2392d0818582cca6d53c
Kabeya243 <kabeyailunga243@gmail.com>
2023-04-27 19:10:42 +0200

    majeure partie du codage termine et reste les messages seulement

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # nanoTeam.github.io
 
 quand j'aurais fin tu pourras tout mofier et le rendre fonctionnel
+CE 27 AVRIL J'AI PRESQUE FIN LE PROJET IL NE RESQUE QUE LE FINISSAGE DE LA CHAT BOX
+
+NE FAIS PAS DE PUSH POUR LE MOMENT 
--- public/script.js
+++ public/script.js
@@ -1,12 +1,15 @@
 // 🤬 POUR LE MOMENT L'HISTORIQUE EST STATIQUE JE VAIS LE RENDRE DYNAMIQUE AVEC JSON 
 // JE VIENS DE MODIFIER LE CODE TOUT ENTIER POUR QUE ME RETROUVE 
-
+let body = document.documentElement;
 let singupPage = document.querySelector('.signup-section');
 let loginPage = document.querySelector('.login-section');
 let connectLink = document.querySelector('.connectLink');
 let signupLink = document.querySelector('.signupLink');
 let menu = document.querySelector('.menu-box');
+let historiquePop = document.querySelector('.historique-pop');
 let menuBtn = document.querySelector('.menu-btn');
+let popBtn = document.querySelector('.bx-dots-vertical-rounded');
+let themeBtn = document.querySelector(".bx-moon");
 
 // script de changement des pages signup et login 
 connectLink.addEventListener('click',() =>{
@@ -21,6 +24,16 @@ signupLink.addEventListener('click',() =>{
 // script du menu 
 menuBtn.addEventListener('click',() =>{
     menu.classList.toggle("active-menu");
- 
 })
+// POP-UP DE PARAMETRE DANS HISTORIQUE 
+popBtn.addEventListener('click',() =>{
+    historiquePop.classList.toggle("active-pop");
+})
+// POP-UP DE PARAMETRE DANS HISTORIQUE 
 // script du menu 
+// DARK MODE SCRIPT 
+themeBtn.addEventListener("click",()=>{
+    body.classList.toggle("dark");
+    themeBtn.classList.toggle("bx-sun")
+})
+// DARK MODE SCRIPT 
